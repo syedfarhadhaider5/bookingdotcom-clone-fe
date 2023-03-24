9693e7585355c0054a7b5ff3c88da5dfd76a589e
Apple <farhad9>
2023-03-24 10:58:10 +0500

    meta head tag added

---
+++ app/Head.tsx
@@ -0,0 +1,13 @@
+function Head() {
+  return (
+    <>
+        <title>Booking.com</title>
+        <meta name="description" content="Book your hotel and flight"></meta>
+        <meta name="keywords" content="Flight, Hotel"></meta>
+        <meta name="author" content="Syed Farhad Haider Shah"></meta>
+        <link  rel="icon" href="/booking.ico" />
+    </>
+  )
+}
+
+export default Head
---
+++ app/hotel/Head.tsx
@@ -0,0 +1,11 @@
+
+export default function Head() {
+  return (
+    <>
+    <title>Booking.com</title>
+    <meta name="description" content="Book your hotel and flight"></meta>
+    <meta name="keywords" content="Flight, Hotel"></meta>
+    <meta name="author" content="Syed Farhad Haider Shah"></meta>
+    <link  rel="icon" href="/booking.ico" />
+</>  )
+}
